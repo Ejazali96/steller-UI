17be37fdfae50bbf3e1ad82213d3ac026c62d259
karimgtc <128481132+karimgtc@users.noreply.github.com>
2023-10-24 03:43:12 +0200

    pipeline.

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+
+  public name: string = "";
+  public email: string = "";
+  public message = "";
+
+  constructor(private http: HttpClient) { }
+
+  public send() {
+
+    this.http.post<any>('https://stellaruiwebsiteapiprod.azurewebsites.net/api/v1/contactformcontroller/contact',
+        { name: this.name, email: this.message, message: this.message })
+        .subscribe(data => {})
+
+    alert('Thanks for the message. It has been sent and we will reply soon. Stellar Security.');
+
+    this.name = "";
+    this.email = "";
+    this.message = "";
+
+  }
 
 }
--- src/app/pages/contact-us/contact-us.module.ts
+++ src/app/pages/contact-us/contact-us.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { ContactUsRoutingModule } from './contact-us-routing.module';
 import { ContactUsComponent } from './contact-us.component';
 import { SharedModule } from './../../shared/shared.module';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     ContactUsComponent
   ],
-  imports: [
-    CommonModule,
-    ContactUsRoutingModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        ContactUsRoutingModule,
+        SharedModule,
+        FormsModule
+    ]
 })
 export class ContactUsModule { }
