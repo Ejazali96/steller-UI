15fb7b0a0115a02d73fc3491d0782e41eec31a52
karimgtc <128481132+karimgtc@users.noreply.github.com>
2023-10-26 23:28:31 +0200

    video

--- src/app/pages/stellar/stellar.component.ts
+++ src/app/pages/stellar/stellar.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class StellarComponent {
 
+  public chosen_phone_subscription = 6;
+
+  public phone_price = "999,99";
+
+  public setChoseSubscription(subscription: number) {
+    this.chosen_phone_subscription = subscription;
+  }
+
 }
