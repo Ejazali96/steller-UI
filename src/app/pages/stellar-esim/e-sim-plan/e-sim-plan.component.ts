853fe9ab7d857425358779af530059a4a20bd294
abdul qayyum <qayyumgb96@gmail.com>
2023-11-03 10:56:16 +0500

    Stellar E-sim page design completed

---
+++ src/app/pages/stellar-esim/e-sim-plan/e-sim-plan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e-sim-plan',
+  templateUrl: './e-sim-plan.component.html',
+  styleUrls: ['./e-sim-plan.component.css']
+})
+export class ESimPlanComponent {
+
+}
---
+++ src/app/pages/stellar-esim/stellar-esim.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stellar-esim',
+  templateUrl: './stellar-esim.component.html',
+  styleUrls: ['./stellar-esim.component.css']
+})
+export class StellarEsimComponent {
+
+}
