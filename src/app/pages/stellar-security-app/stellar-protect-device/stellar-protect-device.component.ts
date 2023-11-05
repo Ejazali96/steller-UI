8a77b90f77b57562030ce2762943bbdc42e54d92
abdul qayyum <qayyumgb96@gmail.com>
2023-11-05 13:16:32 +0500

    stellar security app page design complete

---
+++ src/app/pages/stellar-security-app/stellar-protect-device/stellar-protect-device.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stellar-protect-device',
+  templateUrl: './stellar-protect-device.component.html',
+  styleUrls: ['./stellar-protect-device.component.css']
+})
+export class StellarProtectDeviceComponent {
+
+}
