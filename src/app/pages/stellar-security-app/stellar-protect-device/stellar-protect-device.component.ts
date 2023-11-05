214f4911356f02af302bf61c7dda978a03062d78
abdul qayyum <qayyumgb96@gmail.com>
2023-11-05 19:10:47 +0500

    video added, url updated,logo updated

--- src/app/pages/stellar-security-app/stellar-protect-device/stellar-protect-device.component.ts
+++ src/app/pages/stellar-security-app/stellar-protect-device/stellar-protect-device.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-stellar-protect-device',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./stellar-protect-device.component.css']
 })
 export class StellarProtectDeviceComponent {
+  @ViewChild('myVideo') myVideo: ElementRef
+  constructor(){
 
+  }
+ isPause:boolean = false
+  playVideo(){
+    if(!this.isPause){
+      this.myVideo?.nativeElement.play();
+    }
+    else{
+      this.myVideo?.nativeElement.pause();
+    }
+    this.isPause = !this.isPause
+  }
 }
