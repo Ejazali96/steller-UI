a20faf8d34e0e687e032d6d2026869ca77b094f8
abdul qayyum <qayyumgb96@gmail.com>
2023-11-08 01:57:14 +0500

    stellar notes page design created

---
+++ src/app/pages/stellar-notes/stellar-notes-main/stellar-notes-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stellar-notes-main',
+  templateUrl: './stellar-notes-main.component.html',
+  styleUrls: ['./stellar-notes-main.component.css']
+})
+export class StellarNotesMainComponent {
+
+}
