234090983f91fc05384846533c51d0060b51f604
abdul qayyum <qayyumgb96@gmail.com>
2023-11-11 00:34:50 +0500

    nofound-component module updated

--- src/app/Layouts/main/main.module.ts
+++ src/app/Layouts/main/main.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { SharedModule } from './../../shared/shared.module';
+import { NotFoundComponent } from 'src/app/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
