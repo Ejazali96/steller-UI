46b69f6dc67a6f1198577c8e5abf360e9af4e902
abdul qayyum <qayyumgb96@gmail.com>
2023-11-24 02:27:54 +0500

    checkout 'I’d like it delivered' tab all pages

--- src/app/Layouts/main/main-routing.module.ts
+++ src/app/Layouts/main/main-routing.module.ts
@@ -5,6 +5,7 @@ import { TermsPageComponent } from './../../pages/terms-page/terms-page.componen
 import { PrivacyPageComponent } from './../../pages/privacy-page/privacy-page.component';
 import { CheckoutPageComponent } from './../../pages/checkout-page/checkout-page.component';
 import { NotFoundComponent } from 'src/app/components/not-found/not-found.component';
+import { ThankYouComponent } from 'src/app/pages/thank-you/thank-you.component';
 
 const routes: Routes = [
   {
@@ -23,9 +24,11 @@ const routes: Routes = [
       { path: 'about-us', loadChildren: () => import('../../pages/about-us/about-us.module').then(m => m.AboutUsModule) },
       { path: 'activate-phone', loadChildren: () => import('../../pages/activate-phone/activate-phone.module').then(m => m.ActivatePhoneModule) },
       { path: 'stellar-store', loadChildren: () => import('../../pages/stellar-store/stellar-store.module').then(m => m.StellarStoreModule) },
+      { path: 'checkout', loadChildren: () => import('../../pages/checkout/checkout.module').then(m => m.CheckoutModule) },
       { path: 'terms-page', component:TermsPageComponent},
       { path: 'privacy-page', component:PrivacyPageComponent},
       { path: 'checkout-page', component:CheckoutPageComponent},
+      {path: 'thank-you', component: ThankYouComponent},
       { path: 'stelleros', loadChildren: () => import('../../pages/stelleros/stelleros.module').then(m => m.StellerosModule) },
       {path: '**', component: NotFoundComponent}
     ]
