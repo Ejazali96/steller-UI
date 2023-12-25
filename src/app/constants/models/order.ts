a6aae204a5fbcfe451030fb536ea1cdad8f12007
Your Name <you@example.com>
2023-12-25 01:17:13 +0100

    Country

---
+++ src/app/constants/models/order.ts
@@ -0,0 +1,18 @@
+export interface Order {
+    payment_firstname: string
+    payment_lastname: any
+    payment_address: any
+    payment_city: any
+    payment_postcode: any
+    payment_country: any
+    payment_state: any
+    payment_email: any
+    payment_phone: any
+    order_status_id: any
+    order_total: number
+    user_id: any
+    order_type: number
+    anonymously_contact_type: any
+    anonymously_contact_info: any
+    payment_type: number
+}
