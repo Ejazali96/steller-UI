809f9c82b9cfb92a44490b8939444dc74f56844f
abdul qayyum <qayyumgb96@gmail.com>
2023-12-27 00:59:47 +0500

    bag items check added for product type

--- src/app/pages/bag/bag-items/bag-items.component.ts
+++ src/app/pages/bag/bag-items/bag-items.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bag-items',
   templateUrl: './bag-items.component.html',
   styleUrls: ['./bag-items.component.css']
 })
-export class BagItemsComponent {
+export class BagItemsComponent implements OnInit {
+@Input() productItem:any
 
+
+ngOnInit(): void {
+  console.log('current item',this.productItem)
+}
 }
