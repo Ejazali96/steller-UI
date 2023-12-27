be299b7593b60cd43d3694d75e0b731a37bc1614
Your Name <you@example.com>
2023-12-27 02:03:34 +0100

    El dudu

--- src/app/pages/stellar/stellar.component.ts
+++ src/app/pages/stellar/stellar.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-stellar',
   templateUrl: './stellar.component.html',
   styleUrls: ['./stellar.component.css']
 })
-export class StellarComponent {
+export class StellarComponent implements OnInit {
   metaDescription:string = 'Stellar Phone: Cutting-edge tech, sleek design. Experience power, stunning display, and advanced security in one. Redefine innovation on the go';
   metaTitle:string = 'Stellar Security - Stellar Phone';
   metaKeywords:string = 'Stellar Security, secure phone, stellar phone';
   public chosen_phone_subscription = 6;
 
   public phone_price = "999,99";
+
+  public product_id = "1083fd58-80a4-4ad1-af00-a78547ef82c1";
+
+  public product_variant: any;
+
+  public product : any = null;
+
+
+  constructor(private productservice: ProductService) { }
+
+
+  async ngOnInit() {
+    setTimeout(async () => {
+      await this.getProduct();
+    }, 1000);
+  }
+
+  public async getProduct() {
+      this.productservice.getProductById(this.product_id).subscribe(x => {
+          this.product = x.product;
+      })
+  }
 
   public setChoseSubscription(subscription: number) {
     this.chosen_phone_subscription = subscription;
--- src/app/pages/stellar/stellar.module.ts
+++ src/app/pages/stellar/stellar.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { StellarRoutingModule } from './stellar-routing.module';
 import { StellarComponent } from './stellar.component';
 import { SharedModule } from '../../../app/shared/shared.module';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     StellarComponent
   ],
-  imports: [
-    CommonModule,
-    StellarRoutingModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        StellarRoutingModule,
+        SharedModule,
+        FormsModule
+    ]
 })
 export class StellarModule { }
