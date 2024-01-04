f5e6973af17b510a2cd8c719adba2a4214fbe11c
abdul qayyum <qayyumgb96@gmail.com>
2024-01-05 02:36:49 +0500

    remove www from url

--- server.ts
+++ server.ts
@@ -36,7 +36,18 @@ export function app(): express.Express {
   return server;
 }
 
+
 function run(): void {
+  const express = require('express');
+const app = express();
+app.use((req:any, res:any, next:any) => {
+  if (req.headers.host.startsWith('www.')) {
+    const newHost = req.headers.host.slice(4); // Remove 'www.'
+    return res.redirect(301, `${req.protocol}://${newHost}${req.originalUrl}`);
+  }
+  next();
+});
+
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
